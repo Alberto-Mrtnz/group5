a369d8da99d7adae47626443fb24511b3d4e0ee6
lesliediaz16 <leslie.diaz2025@poma.superate.org.sv>
2024-05-14 14:03:29 -0600

    LOGIN
    LOGIN CHANGE

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,24 +1,28 @@
 <x-layout>
-<div class="mt-44 ml-96 mr-80 float-end text-center">
-<form action="/login" method="POST">
-    @csrf 
-    <h1 class="mb-11 text-2xl ">EVYS</h1>
-    <div class="mb-2">
-    <x-input titulo="Nombre" type="text" nombre="nomre"></x-input>
-    </div>
-    <div class="mb-2">
-    <x-input titulo="Email" type="email" nombre="email"></x-input>
-    </div>
-    <div class="mb-2">
-    <x-input titulo="Contraseña" type="password" nombre="contraseña"></x-input>
-    </div>
-    <div>
-    <x-input titulo="Fecha de nacimiento" type="date" nombre="fecha de nacimiento"></x-input>
-    </div>
-    <div class="mt-11">
-            <x-buttom type="push" titulo="REGISTER" nombre="REGISTER "/>
+<div class="grid grid-cols-3 place-items-center">
+
+    <div class="" style="">
+        <img src="{{ asset('img/LOGIN.jpg') }}" class="h-screen bg-right">
     </div>
 
-</form>
+    <div class="cols-span-2">
+    <form action="/login" method="POST">
+    @csrf
+        <h1 class="mb-11 text-2xl text-center">EVYS</h1>
+
+        <div class="mb-2">
+            <x-input titulo="Nombre" type="text" nombre="nomre"></x-input>
+            <x-input titulo="Email" type="email" nombre="email"></x-input>
+            <x-input titulo="Contraseña" type="password" nombre="contraseña"></x-input>
+            <x-input titulo="Fecha de nacimiento" type="date" nombre="fecha de nacimiento"></x-input>
+        </div>
+
+        <div>
+            <x-buttom>REGISTER</x-buttom>
+        </div>
+
+    </form>
+    </div>
+
 </div>
 </x-layout>

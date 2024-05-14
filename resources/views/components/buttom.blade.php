9e8883cda955d25f44c91403d8909bf753dc4bf4
Alberto-Mrtnz <153474873+Alberto-Mrtnz@users.noreply.github.com>
2024-05-14 14:36:22 -0600

    Login
    login

--- resources/views/components/buttom.blade.php
+++ resources/views/components/buttom.blade.php
@@ -1,6 +1,6 @@
 
-<div>
-<button class="bg-stone-300 rounded-lg font-medium p-2">
+<div class="text-center mt-11">
+<button class="bg-stone-300 rounded-lg font-medium p-2 size-16">
 {{$slot}}
 </button>
 </div>

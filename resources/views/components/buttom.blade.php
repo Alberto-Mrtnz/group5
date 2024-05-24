03cb4d6ab64509c7f401fe2f26289d7418f98977
Alberto-Mrtnz <153474873+Alberto-Mrtnz@users.noreply.github.com>
2024-05-23 22:39:31 -0600

    HOMEPAGE
    homepage

--- resources/views/components/buttom.blade.php
+++ resources/views/components/buttom.blade.php
@@ -1,6 +1,4 @@
-
-
-<div class=" mt-11 font-montserrat text-2xl text-center">
+<div class="mt-11 font-montserrat text-2xl text-center">
 <button class="bg-stone-300 rounded-lg font-medium p-2">
 {{$slot}}
 </button>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,28 @@
-<x-layout>
+ <x-layout>
 <div>
+
+    <nav class="py-6 relative">
+        <div class="container mx-auto flex">
+
+            <h1 class="text-3xl font-moon flex-grow">EVYS</h1>
+
+            <div class="self-center flex flex-grow justify-between">
+                <div>
+                    <a href="" class="font-montserrat hover:border-b-4 border-zinc-900">Acerca de</a>
+                    <a href="" class="font-montserrat hover:border-b-4 border-zinc-900 mx-8">Nuestro servicios</a>
+                    <a href="" class="font-montserrat hover:border-b-4 border-zinc-900">Contactos</a>
+                </div>
+                <div>
+                    <a href="" class="font-montserrat hover:text-gray-500">Iniciar sesión</a>
+                    <a href="" class="font-montserrat bg-purple-50 border border-transparent rounded-lg p-2 hover:bg-purple-100">Registrarse</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="" style="">
+        <img src="{{ asset('img/fondo-homepage.jpg') }}" class="h-full w-full ">
+    </div>
 
 </div>
 </x-layout>

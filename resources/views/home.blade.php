645ef3058ecada3f8cfd61e85ef954139aad12fc
lesliediaz16 <leslie.diaz2025@poma.superate.org.sv>
2024-06-07 15:45:08 -0600

    PRICIPAL
    CHANGE IN THE PRINCIPAL VIEW

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,39 @@
 <x-layout>
     <div class="relative">
-        <nav class="bg-inherit py-6 fixed w-full top-0 bg-gray-300 ">
-            <div class="container mx-auto flex ">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-                <h1 class="text-3xl font-moon flex-grow">EVYS</h1>
-            </div>
-
+<!-- barra -->
+    <div class="">
+        <nav class="w-full bg-gray-400 grid-cols-2">
+        <div class="inline-flex ">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        <h1 class="text-3xl font-moon flex-grow text-black">EVYS</h1>
+    </div class>
+        <div class="">
             <div>
-                <label class="">
-                <div class="self center flex flex-grow">
-                <span class="sr-only">Search</span>
-                <input class="text-gray-400 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm sm:text-sm" placeholder="Search..." type="text"/>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
-                </div>
-                </label>
+                <img class=" h-10 w-10 rounded-full content-between" src="{{ asset('img/persona2.jpg') }}">
             </div>
-
-            <div class="inline-flex">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m80-80 200-560 360 360L80-80Zm132-132 282-100-182-182-100 282Zm370-246-42-42 224-224q32-32 77-32t77 32l24 24-42 42-24-24q-14-14-35-14t-35 14L582-458ZM422-618l-42-42 24-24q14-14 14-34t-14-34l-26-26 42-42 26 26q32 32 32 76t-32 76l-24 24Zm80 80-42-42 144-144q14-14 14-35t-14-35l-64-64 42-42 64 64q32 32 32 77t-32 77L502-538Zm160 160-42-42 64-64q32-32 77-32t77 32l64 64-42 42-64-64q-14-14-35-14t-35 14l-64 64ZM212-212Z"/></svg>
-              <x-buttom3>Local</x-buttom3>
-            </div>
+        </div>
         </nav>
-
     </div>
-<!-- Targeta -->
+<!-- buscador -->
+            <label class="relative block px-20 pt-5">
+                <span class="sr-only">Search</span>
+                    <span class="absolute inset-y-0 left-0 flex items-center">
+                        <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+                    </span>
+                <div class="inline-flex grid-cols-2 w-full">
+                    <input class="placeholder:text-slate-400 block col-span-1 justify-end bg-white w-full border border-slate-300 rounded-full py-2 pl-9 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search..." type="text" name="search"/>
+                    <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white shadow-sm bg-white rounded-full border border-slate-300 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                        <span class="sr-only">Search</span>
+                    </button>
+                </div>
+            </label>
+<!-- categorias -->
+            <x-buttonicon></x-buttonicon>
+<!-- tarjeta -->
+            <x-event></x-event>
+        </div>
 
 </x-layout>
-
-

a0d9326f99f4269e20b501005247322cd78b8190
Alberto-Mrtnz <153474873+Alberto-Mrtnz@users.noreply.github.com>
2024-06-12 13:57:54 -0600

    routes update

--- routes/get.php
+++ routes/get.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/login', function () {
-    return view('login');
-});
+    return view('users.login');
+})->name('login');
 
 Route::get('/register', function () {
-    return view('register');
+    return view('users.register');
 });
 
 Route::get('/home', function () {

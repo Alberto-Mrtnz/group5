22d15f343fce2ec42706dbea1181282b22e3fbaa
Alberto-Mrtnz <153474873+Alberto-Mrtnz@users.noreply.github.com>
2024-06-14 15:52:15 -0600

    homepage
    hhome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
          <nav class="py-6 fixed w-full top-0 z-40">
              <div class="container mx-auto flex text-white">
 
-                 <h1 class="text-3xl font-moon flex-grow">EVYS</h1>
+                 <h1 class="text-3xl font-moon flex-grow">FIKA</h1>
 
                  <div class="self-center flex flex-grow justify-between">
                      <div class="space-x-6">
                          <a href="#aboutUs" class="font-montserrat hover:border-b-4 border-white">Acerca de</a>
-                         <a href="" class="font-montserrat hover:border-b-4 border-white">Nuestro servicios</a>
+                         <a href="#ourServices" class="font-montserrat hover:border-b-4 border-white">Nuestro
+                             servicios</a>
                          <a href="" class="font-montserrat hover:border-b-4 border-white">Contactos</a>
                      </div>
                      <div class="space-x-4">
@@ -28,27 +29,44 @@
              </div>
          </div>
 
-         <div id="aboutUs" class="bg-gray-200 grid grid-cols-5">
-             <div class="col-span-3 justify-center text-center mx-44 my-44 border border-transparent bg-gray-300">
-                 <h2 class="font-montserrat font-semibold text-center text-2xl pb-2.5">¿Quiénes somos?</h2>
-                 <h2 class="font-montserrat text-xl">Facilitar el proceso de organización de eventos y celebraciones a
+         <div id="aboutUs" class="bg-gray-200 py-16 flex flex-col justify-end items-end">
+             <div class="absolute justify-center text-center border border-transparent bg-gray-300 w-5">
+                <h2 class="font-montserrat font-semibold text-center text-2xl pb-2.5">¿Quiénes somos?</h2>
+                <h2 class="font-montserrat text-xl">Facilitar el proceso de organización de eventos y celebraciones a
                      los salvadoreños mediante el desarrollo de una plataforma online que optimice la búsqueda y
                      contratación de servicios relacionados con eventos para que la gente pueda ahorrar dinero y
                      personalizar sus eventos por su cuenta.</h2>
              </div>
-             <div class="col-span-2">
-                 <img src="{{ asset('img/salones-para-eventos.png') }}" class="my-36 mr-12">
+
+             <div class="static flex items-center justify-center w-2/5">
+                 <img src="{{ asset('img/imagen restaunate.jpg') }}" class="">
              </div>
          </div>
 
-         <div class="bg-gray-300">
+         <div id="ourServices" class="bg-gray-300">
              <h1 class="font-montserrat font-semibold text-center text-2xl">Nuestros servicios</h1>
-             <swiper-container class="place-items-center">
+             <swiper-container class="flex items-center justify-center w-64">
                  <swiper-slide><img src="{{ asset('img/salones-para-eventos.png') }}"></swiper-slide>
                  <swiper-slide><img src="{{ asset('img/salones-para-eventos.png') }}"></swiper-slide>
                  <swiper-slide><img src="{{ asset('img/salones-para-eventos.png') }}"></swiper-slide>
              </swiper-container>
          </div>
 
+         <section class="bg-gray-400 grid grid-cols-3">
+            <div class="col-span-1">
+                <h1 class="text-5xl font-moon text-center py-24">FIKA</h1>
+            </div>
+            <div class="font-medium font-montserrat col-span-1">
+                <h2 class="my-8">Contactos:</h2>
+                <h2 class="mb-1">+503 67982315</h2>
+                <h2>Fika_eventos@gmail.com</h2>
+            </div>
+            <div class="font-medium font-montserrat col-span-1">
+                <h2 class="my-8">Síguenos en redes sociales:</h2>
+            </div>
+            <div class="font-medium font-montserrat mx-4 border-t border-gray-200 col-span-3">
+                <h1 class="my-4">Copyright © 2024 Fika - Privacidad - Términos</h1>
+            </div>
+         </section>
      </div>
  </x-layout>

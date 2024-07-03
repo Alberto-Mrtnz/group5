d0af3622c1aa7ba588f1dca884c5f8998a69cf7a
Michel-Montoya <hazel.montoya2025@poma.superate.org.sv>
2024-07-02 21:01:08 -0600

    welcome,login,register

--- resources/views/components/buttom.blade.php
+++ resources/views/components/buttom.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center">
 
-    <button class="bg-fika-green rounded-lg font-medium p-2  mt-11 font-montserrat text-2xl text-center" type="submit"
+    <button class="p-2 text-2xl font-medium text-center rounded-lg bg-fika-turquesa mt-11 font-montserrat" type="submit"
     {{ $attributes }}>
     {{ $slot }}
 </button>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
     <title>Document</title>
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
-<body class="bg-fika-lavander">
+<body class="bg-gray-100">
     {{$slot}}
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
 </body>

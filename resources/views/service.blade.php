d864e05a0113ea66d8ec5953d05c382b21c3d140
Alberto-Mrtnz <153474873+Alberto-Mrtnz@users.noreply.github.com>
2024-08-27 14:38:18 -0600

    review

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -84,12 +84,47 @@
         </div>
 
         <div class="grid grid-cols-2">
-            <div class="col-span-1 bg-red-800">
-                <h1>JUAN PABLO ll</h1>
-                <img class="w-10 h-10 rounded-full" src="{{ asset('img/persona2.jpg') }}">
-            </div>
-            <div class="col-span-1">
-                <h1>JUAN PABLO ll</h1>
+            <div class="col-span-1 bg-gray-200 p-2">
+                <h1 class="py-1.5">JUAN PABLO ll</h1>
+                <div class="flex space-x-2">
+                    <img class="w-10 h-10 rounded-full" src="{{ asset('img/persona2.jpg') }}">
+                    <h1>"The Eventi website has an excellent design that benefits
+                        vendors as well as customers. It was simple for me as a customer
+                         to look through locations, weigh services, and make judgments.
+                        For vendors, this is a great way to present their products.</h1>
+                </div>
+                <div class="my- ml-">
+                    <div x-data="{
+                        rating: 0,
+                        hoverRating: 0,
+                        ratings: 5,
+                        rate(amount) {
+                            if (this.rating == amount) {
+                                this.rating = 0;
+                            } else this.rating = amount;
+                        }
+                    }"
+                        class="">
+                        <div class="flex space-x-0">
+                            <template x-for="star in ratings" :key="star">
+                                <button @click="rate(star)" @mouseover="hoverRating = star"
+                                    @mouseleave="hoverRating = rating" aria-hidden="true" :title="star"
+                                    class="rounded-sm text-gray-400 fill-current focus:outline-none focus:shadow-outline p-1 w-12 m-0 cursor-pointer"
+
+                                    :class="{ 'text-gray-600': hoverRating >= star,
+                                    'text-yellow-400': rating >= star &&
+                                            hoverRating >= star }">
+
+                                    <svg class="w-15 transition duration-150" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20" fill="currentColor">
+                                        <path
+                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                </button>
+                            </template>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>

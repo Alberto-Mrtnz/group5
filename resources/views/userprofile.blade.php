1b9c17bec3838782d66167bec48b47bdcb8ae37c
Alberto-Mrtnz <153474873+Alberto-Mrtnz@users.noreply.github.com>
2024-09-13 14:54:09 -0600

    userprofile

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -1,27 +1,22 @@
 <x-layout>
     <div x-data="{ opened: false }">
         <x-barra/>
-            <div class="grid grid-cols-2 m-2 border-2x place-items-center">
-                <div class="grid py-16 cols-start-2">
-                    <div class="pl-8">
-                    </div>
-                </div>
-
                 <div>
-                    <div class="p-6 rounded-lg mt-7 bg-slate-300">
-                        <h1 class="font-moon font-extrabold text-4xl mb-4">Provider contact information</h1>
-                        <div class="flex">
-                            <img class="w-14 rounded-full" src="{{ asset('img/icon.png') }}">
-                            <p class="p-4 rounded-lg w-80 bg-fika-piel">{{ $user->name }}</p>
-                        </div>
+                    <div class="flex justify-center">
+                        <img class="w-48 m-8 rounded-full" src="{{ asset('img/icon.png') }}">
                     </div>
-
-                    <p class="p-4 rounded-lg w-80 bg-fika-piel">{{ $user->email }}</p>
-                    <p class="p-4 rounded-lg w-80 bg-fika-piel">{{ $user->phone }}</p>
+                    <div class="text-center justify-center">
+                        <h1 class="font-moon font-extrabold text-4xl">Personal information</h1>
+                    </div>
+                    <div class="text-center">
+                        <p class="my-2">{{ $user->name }}</p>
+                        <p class="my-2">{{ $user->email }}</p>
+                        <p class="my-2">{{ $user->phone }}</p>
+                    </div>
                 </div>
-            </div>
+
         @foreach($tableros as $tablero)
-        {{ $tablero }}
+            {{ $tablero }}
 
         @endforeach
     </div>
